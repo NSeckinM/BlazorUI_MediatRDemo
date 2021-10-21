08ce9ce57f26c054d66167900efd47ce12bf5eb3
NseckinM <seckinmantar@gmail.com>
2021-10-21 19:06:55 +0300

    Readed People from list and showed on index page

---
+++ DemoLibrary/Queries/GetPersonListQuery.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Queries
+{
+    // record and class equivalent except the fact that a record has the additional syntax and goodness
+    // that records provide such as beign immutable by default
+
+    //public class GetPersonListQueryClass : IRequest<List<PersonModel>>
+    //{
+
+    //}
+
+    public record GetPersonListQuery() : IRequest<List<PersonModel>>;
+
+}
